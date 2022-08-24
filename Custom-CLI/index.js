421d1bc46a428b4f6e1885339ba9be47f9171e83
mantowong <81311669+mantowong@users.noreply.github.com>
2022-08-24 22:09:31 +0200

    Affirmation API implementiert

--- Custom-CLI/index.js
+++ Custom-CLI/index.js
@@ -10,14 +10,22 @@
 const init = require('./utils/init');
 const cli = require('./utils/cli');
 const log = require('./utils/log');
-
+const axios = require('axios');
 const input = cli.input;
 const flags = cli.flags;
 const { clear, debug } = flags;
 
+
 (async () => {
 	init({ clear });
 	input.includes(`help`) && cli.showHelp(0);
-
 	debug && log(flags);
+
+if(input.includes('affirmation')) {
+	const res = await axio.get('https://www.affirmations.dev/');
+	console.log(res.data.affirmation)
+ ;
+}
 })();
+
+
